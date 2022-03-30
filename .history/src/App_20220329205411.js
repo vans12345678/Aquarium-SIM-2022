a25c3f4ed01bf461afbeabf1c2b3dfc326e36698
jacky yuan <flamingknight11@hotmail.com>
2022-03-30 01:15:21 -0400

    created compendium and fish profiles

---
+++ .history/src/App_20220329205411.js
@@ -0,0 +1,41 @@
+import NavBar from "./Navbar";
+import Home from "./Home";
+import Aquarium from "./Aquarium";
+import Compendium from "./Compendium";
+import Listings from "./Listings";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  const title = "Welcome to the new blog";
+
+  return (
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/aquarium">
+              <Aquarium />
+            </Route>
+            <Route path="/Compendium">
+              <Compendium />
+            </Route>
+            <Route path="/Compendium/:FishProfile" element={<FishProfile/>}>
+              <FishProfile />
+            </Route>
+            <Route path="/listings">
+              <Listings />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ .history/src/FishProfile_20220330002551.js
@@ -0,0 +1,101 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import React from "react";
+import { Table } from "react-bootstrap";
+import Axios from "axios";
+import placeholder from "./images/placeholder.png";
+
+function getFishName () {
+  const currentFishURL = window.location.pathname;
+  const temp = currentFishURL.split("/", 3);
+  const temp2 = temp[2].split("%20");
+  var currentFishName = "";
+  if(temp2.length > 1)
+  {
+      for(let j = 0; j < temp2.length; j++)
+      {
+          currentFishName += " " + temp2[j];
+      }
+  }
+
+ return currentFishName;
+}
+
+const FishProfile = () => {
+
+  var [fishInfo, setFishInfo] = useState([]);
+  var infoArray = fishInfo;
+  const getCurrentFish = () => {
+    if (fishInfo.length === 0){
+    Axios.post("http://localhost:3001/currentFishGet", { currentFishName: getFishName() }).then((response) => {
+      setFishInfo(response.data);
+    });
+  }
+  }
+
+  return (
+    <div>
+      
+      <section className="homeMiddle" onLoad={getCurrentFish()}>
+      {fishInfo.map((val, key) => {
+        return(
+          <div key = {val.fishID}>
+          <h1>{val.fishCommonName}</h1>
+          <img className="aquarium"
+          width="500px"
+          height="300px"
+          src={ require('./images/'+ infoArray[12]) } 
+          alt=""/>
+          <Table           
+          striped
+            bordered
+            hover
+            variant="dark"
+            className="w-50 text-center">
+            <tbody>
+          <tr>
+            <td>Common Name: </td>
+            <td>{val.fishCommonName}</td>
+            </tr>
+            <tr>
+            <td>Scientific Name: </td>
+            <td>{val.fishScientificName}</td>
+            </tr>
+            <tr>
+              <td>Average Size: </td>
+              <td>{val.fishAverageSize} cm</td>
+            </tr>
+            <tr>
+              <td>Recommended PH: </td>
+              <td>{val.fishLowerPH} - {val.fishUpperPH}</td>
+            </tr>
+            <tr>
+              <td>Recommended Temperature: </td>
+              <td>{val.fishLowerTemp} - {val.fishUpperTemp} °C</td>
+            </tr>
+            <tr>
+              <td>Aggression to its own species: </td>
+              <td>{val.fishAggrSameSpecies}</td>
+            </tr>
+            <tr>
+              <td>Aggression to its other species: </td>
+              <td>{val.fishAggrOtherSpecies}</td>
+            </tr>
+            <tr>
+              <td>Location in the tank: </td>
+              <td>{val.fishLocationTank}</td>
+            </tr>
+            </tbody>
+          </Table>
+          </div>
+        )
+              })}
+      </section>
+      <hr/>
+      {console.log("hello")}
+              {console.log(infoArray)}
+    </div>
+  );
+};
+
+export default FishProfile;
